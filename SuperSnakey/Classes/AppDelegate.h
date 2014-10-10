74ca5d423a30d93d30643e6d08bad4494d22fab2
strin <stl501@gmail.com>
2014-10-10 20:44:24 +0800

    Initial Commit

---
+++ SuperSnakey/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  SuperSnakey
+//
+//  Created by Tianlin Shi on 10/10/14.
+//  Copyright Tianlin Shi 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ SuperSnakey/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SuperSnakey
+//
+//  Created by Tianlin Shi on 10/10/14.
+//  Copyright Tianlin Shi 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
